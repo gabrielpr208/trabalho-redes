8448c7c37f53a5f9ade82d56d823f9e44e885448
gabrielpr208 <gabriel.rodrigues.gpr@gmail.com>
2025-11-27 18:08:37 -0300

    feat: main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
 import asyncio
 from p2pClient import P2PClient
-from config import MY_PEER_ID
-from config import MY_LISTEN_PORT
 
 try:
-    print("------------------------")
-    print(f"Peer id: {MY_PEER_ID}")
-    print(f"Peer listen port: {MY_LISTEN_PORT}")
-    print("------------------------")
     asyncio.run(P2PClient().start())
 except KeyboardInterrupt:
     print("Encerrado")
