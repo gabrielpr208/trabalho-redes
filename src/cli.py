589ecf611b0af38a2c167417f301d6d8b5c7c310
DanielOliMorais <oliveiramoraisdaniel1@gmail.com>
2025-11-30 13:24:57 -0300

    fix issues

--- src/cli.py
+++ src/cli.py
@@ -82,7 +82,7 @@ class Cli:
             await self.p2p_client.print_rtt()
 
         elif cmd == "/quit":
-            await self.rendezvous.unregister()
+            await self.p2p_client.rendezvous.unregister()
             self.stop()
         else:
             print(f"Comando {cmd} não é válido")
